d2de872c1f2c7dde3354e480c5d1aee324c24825
Elgs Qian Chen <elgs1980@hotmail.com>
2018-11-30 22:18:14 -0800

    index page works now.

--- src/index/index.js
+++ src/index/index.js
@@ -1,10 +1,33 @@
 import './index.scss';
+import {
+    parseDOMElement,
+} from '../utilities/utilities.js';
 import modules from './modules.json';
 
 window.onload = () => {
-    const buildTime = document.querySelector('span.buildTime');
+    const buildTime = document.querySelector('.azui span.buildTime');
     buildTime.innerHTML = modules.buildTime;
 
+    const moduleList = document.querySelector('.azui div.moduleList');
+    Object.keys(modules.modules).map(m => {
+        const mo = modules.modules[m];
+        if (mo.type > 0 && mo.type < 4) {
+            const moduleTpl = `<div class="modules">
+        <h5>${m}</h5>
+        <ul></ul>
+        </div>`;
+            const moduleDOM = parseDOMElement(moduleTpl)[0];
 
-    console.log(modules);
+            mo.pages.map(page => {
+                const pageLiTpl = `<li><a href='${page}'>${page}</a></li>`
+                const pageLi = parseDOMElement(pageLiTpl)[0];
+                const ul = moduleDOM.querySelector('ul');
+                ul.appendChild(pageLi);
+            });
+
+            moduleList.appendChild(moduleDOM);
+        }
+    });
+
+    // console.log(modules);
 };
