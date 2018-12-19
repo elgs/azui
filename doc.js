c65fbae2a57525744237117a10a3d2fb6324ea23
Elgs Qian Chen <elgs1980@hotmail.com>
2018-12-18 16:55:18 -0800

    doc gen starts to work now.

--- doc.js
+++ doc.js
@@ -5,6 +5,8 @@ class A {
             width: 400, // azDoc:width: width of the div
             height: 300, // azDoc:height: height of the div
             x: _ => {}, //azDoc:x: x value
+            y: (a, b, c) => {}, //azDoc:y: y value
+            z: function (x, y, z) {}, //azDoc:z: z value
             // azDoc:settings:end
         }, options);
         this.settings = settings;
@@ -36,14 +38,26 @@ class A {
             }
         }));
     }
-    f2(c) {}
+    // azDoc:method:start
+    f2(x = 'abc') {
+        // azDoc: f2 does some cneaky things
+        // azDoc:x: description of x
+        // azDoc:method:end
+    }
 }
 
 const classStr = A.toString();
 const getRe = type => `\\/\\/\\s*azDoc\\:${type}\\:start([\\s\\S]*?)\\/\\/\\s*azDoc\\:${type}\\:end`;
 
 const parseFunction = f => {
-    return f.toString();
+    const re = /(\(.*?\))/gm;
+    const fstr = f.toString().trim();
+    // console.log(fstr);
+    let match = re.exec(fstr);
+    if (match !== null) {
+        return match[0];
+    }
+    return '()';
 };
 
 const parseMethods = str => {
@@ -62,7 +76,18 @@ const parseMethods = str => {
         let match1 = re1.exec(s);
         if (match1 !== null) {
             const s1 = match1[0].trim();
-            method.declaration = s1;
+            const re2 = /(.*?)\s*\((.*?)\)/gm;
+            let match2 = re2.exec(s1);
+            if (match2 !== null) {
+                const key = match2[1].trim();
+                const dict = match2[2].trim();
+                method.key = key;
+                // method.dict = s2[2];
+                if (dict.length > 0) {
+                    eval.call(global, 'var ' + dict + ';');
+
+                }
+            }
         }
 
         // parse method docs
@@ -73,14 +98,16 @@ const parseMethods = str => {
             const s2 = match2[1].trim();
             match2 = re2.exec(s);
             if (firstLine) {
-                method.doc = s2;
+                method.desc = s2;
             } else {
                 const parts = s2.split(/\:(.*)/);
                 const key = parts[0].trim();
                 const desc = parts[1].trim();
                 method.params.push({
                     key,
-                    desc
+                    desc,
+                    defaultValue: global[key],
+                    type: typeof global[key],
                 });
             }
             firstLine = false;
@@ -153,7 +180,7 @@ const parseEvents = str => {
             const key = parts[0].trim();
             const desc = parts[1].trim();
             if (firstLine) {
-                event.name = key;
+                event.key = key;
                 event.desc = desc;
             } else {
                 event.params.push({
